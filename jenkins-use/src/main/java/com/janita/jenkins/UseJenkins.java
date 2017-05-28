b76e3e73d4a70bd03d146e6d2b078aaaaeca2e03
janforp <janforp@163.com>
2017-05-28 21:48:09 +0800

    jenkins的使用配置

--- jenkins-use/src/main/java/com/janita/jenkins/UseJenkins.java
+++ jenkins-use/src/main/java/com/janita/jenkins/UseJenkins.java
@@ -7,4 +7,32 @@ package com.janita.jenkins;
  * 搭建jenkins的过程见 jenkins-install 模块
  */
 public class UseJenkins {
+
+
+    /**
+     * 1.首次访问安装好的 jenkins ,点击系统管理,见图1
+     *
+     * 2.点击 Global Tool Configuration”,见图2
+     *
+     * 3.配置一些全局工具,见图 2.1 及图2.2
+     * 配置完点击 保存
+     *
+     * 4.配置邮箱,见图3
+     */
+
+
+    /**
+     * 新建任务
+     * 1. 图4
+     *
+     *
+     * 2.但是点击保存的时候 ,导致jenkins无法访问
+     * 是因为服务器上没有公钥跟私钥,需要生成,把公钥拷到 gitlab 上面,见图5
+     *
+     *
+     *
+     *
+     */
+
+
 }
